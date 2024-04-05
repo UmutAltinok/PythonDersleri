4b0e7c2d83daa8e6e188afc2fe207e3854e13549
Umut Altınok <165695467+UmutAltinok@users.noreply.github.com>
2024-04-05 14:51:55 +0300

    Add files via upload

---
+++ PYQT6.py
@@ -0,0 +1,72 @@
+from PyQt6.QtWidgets import *
+
+# ex1
+# app = QApplication([])
+
+# window=QWidget()
+# window.show()
+# app.exec()
+
+# ex2
+# app = QApplication(sys.argv)
+# window = QMainWindow()
+# window.show()
+# # Start the event loop.
+# app.exec()
+
+# ex3
+# import sys
+# app = QApplication(sys.argv)
+# window = QPushButton("Tıkla")
+# window.show()
+# app.exec()
+
+
+# ex4
+# # Uygulama oluşturma
+# app = QApplication([])
+# # buraya araçlar(widgets) ekleme
+# label = QLabel('Merhaba!')
+# label.show()
+# # Uygulamayı çalıştırma
+# app.exec()
+
+# ex5
+# import sys
+# app = QApplication(sys.argv)
+
+# x = QWidget()
+# x.show()  
+# window1 = QPushButton("Tıkla")
+# window1.show()  
+# aa = QLabel("Merhaba")
+# aa.show()
+
+# app.exec()
+# deneyebilirsiniz.
+# pencere.setWindowTitle('Deneme')
+# pencere.resize(300,50)
+# pencere.setFixedSize(100, 100)
+
+
+# uygulama=QApplication([])
+# pencere=QMainWindow()
+# pencere.setWindowTitle("Zeynomla Uygulama")
+
+# # icerik=QHBoxLayout() #Horizontal bir kutucuk oluşturur
+# icerik=QVBoxLayout() #Vertical bir kutucuk oluşturur
+# icerik.addWidget(QLabel("Çevrilecek: "))
+# icerik.addWidget(QLineEdit())
+# icerik.addWidget(QPushButton("Çevir"))
+# icerik.addWidget(QLabel("Sonuç: "))
+
+# araclar = QWidget()
+# araclar.setLayout(icerik)
+# pencere.setCentralWidget(araclar)
+# pencere.show()
+
+# uygulama.exec()
+
+
+
+
